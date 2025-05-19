94ac6229e201cdd5d20d37dc514af98bf3d285df
crtmss <nicklol225@gmail.com>
2025-05-19 22:23:13 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import WorldScene from './scenes/WorldScene.js';
 import CombatScene from './scenes/CombatScene.js';
 
 const config = {
-    type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
-    backgroundColor: '#000000',
-    scene: [LobbyScene, WorldScene, CombatScene],
-    parent: 'game-container',
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  backgroundColor: '#000000',
+  parent: 'game-container',
+  scene: [LobbyScene, WorldScene, CombatScene],
+  dom: {
+    createContainer: true  // 👈 this is the missing piece
+  }
 };
 
 const game = new Phaser.Game(config);
