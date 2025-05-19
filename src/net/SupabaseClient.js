8d0ba13afee6d8277544306cc91759edfa4e83b9
crtmss <nicklol225@gmail.com>
2025-05-19 21:26:35 +0200

    Update SupabaseClient.js

--- src/net/SupabaseClient.js
+++ src/net/SupabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 export const supabase = createClient(
-    'https://your-project.supabase.co',
-    'public-anon-key'
+    'https://pcdveqprfopaofcjkady.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjZHZlcXByZm9wYW9mY2prYWR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNDMyMDksImV4cCI6MjA2MDYxOTIwOX0.YYffphzHl9CtG6L9XpEBLgFE9WfYSq_F-RT3cg10d_k'
 );
