fa5d214719aee0124b9ae15b4e15086c31691afb
crtmss <nicklol225@gmail.com>
2025-06-07 18:07:57 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
+// deephexbeta/src/main.js
+
 import LobbyScene from './scenes/LobbyScene.js';
 import WorldScene from './scenes/WorldScene.js';
 import CombatScene from './scenes/CombatScene.js';
 
 const config = {
   type: Phaser.AUTO,
-  width: 1280,
-  height: 720,
+  width: 1600, // Increased from 1280
+  height: 1000, // Increased from 720
   backgroundColor: '#000000',
   parent: 'game-container',
   scene: [LobbyScene, WorldScene, CombatScene],
   dom: {
-    createContainer: true  // 👈 this is the missing piece
+    createContainer: true
   }
 };
 
